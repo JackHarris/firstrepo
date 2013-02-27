65351068fce13a82c1f5c517a47037ca020f15c0
Jack Harris <jackfishbluefish@aol.com>
2013-02-27 16:57:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jackharris::Application.config.secret_token = 'c8920d7821dc4bbdf060e707fd86f1b1a37345239fd5cfc3ff09903c9638e50f4975399d20e9568c5176dc84bac7ac574e8fb631f471238011db3c3c5b3314f1'
